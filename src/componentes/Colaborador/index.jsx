5e08ed82dbb2d9666ac66ce22bd41826065ad581
Raniel Lopes <raniel.ldms@gmail.com>
2024-12-19 13:46:09 -0300

    finalizando a parte de time

--- src/componentes/Colaborador/index.jsx
+++ src/componentes/Colaborador/index.jsx
@@ -1,13 +1,13 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ({nome, imagem, cargo}) => {
     return (<div className='colaborador'>
         <div className='cabecalho'>
-            <img src='https://github.com/raniel-lopes.png' alt=''/>
+            <img src={imagem} alt={nome} />
         </div>
         <div className='rodape'>
-            <h4>Raniel Lopes</h4>
-            <h5>Front</h5>
+            <h4>{nome}</h4>
+            <h5>{cargo}</h5>
         </div>
     </div>)
 }
--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -19,6 +19,10 @@ const Formulario = (props) => {
             imagem,
             time 
         })
+        setNome('')
+        setCargo('')
+        setCargo('')
+        setTime('')
     }
 
     return (
