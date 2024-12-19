e4ada773d68954a1425385ba6ceb1454de9687d5
Raniel Lopes <raniel.ldms@gmail.com>
2024-12-19 12:36:16 -0300

    criando de forma estatica os colaboradores

---
+++ src/componentes/Colaborador/index.jsx
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/raniel-lopes.png' alt=''/>
+        </div>
+        <div className='rodape'>
+            <h4>Raniel Lopes</h4>
+            <h5>Front</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
