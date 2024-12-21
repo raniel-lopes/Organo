45a18b0d554916551e3a1eda6c515e2f543d89be
Raniel Lopes <raniel.ldms@gmail.com>
2024-12-21 17:19:06 -0300

    corrigindo comiit

--- src/componentes/Time/index.jsx
+++ src/componentes/Time/index.jsx
@@ -1,5 +1,7 @@
 import Colaborador from '../Colaborador'
 import './Time.css'
+import hexToRgba from 'hex-to-rgba';
+
 
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
